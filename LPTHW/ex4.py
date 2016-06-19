035259fab11be45efb7d4684d4a22227d00de48b
AmmarAliShah <syedammarali24@gmail.com>
2016-06-20 01:19:14 +0500

    Day 02

--- LPTHW/ex4.py
+++ LPTHW/ex4.py
@@ -1,10 +1,18 @@
+# creates variable car and sets it to 100
 cars = 100
+# creates variable space_in_a_car and sets it to 4
 space_in_a_car = 4.0
+# creates variable drivers and sets it to 30
 drivers = 30
+# creates variable passengers and sets it to 90
 passengers = 90
+# creates variable cars_not_driven and sets it to the answer of var cars minus drivers
 cars_not_driven = cars - drivers
+# creates variable cars_driven and sets it to the value of variable drivers
 cars_driven = drivers
+# creates variable carpool_capacity and sets it to the product of var cars_driven and space_in_a_car
 carpool_capacity = cars_driven * space_in_a_car
+# creates variable average_passengers_per_car and sets it to the result of var passengers divided by var cars_driven
 average_passengers_per_car = passengers / cars_driven
 
 
